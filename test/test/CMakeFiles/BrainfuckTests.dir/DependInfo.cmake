ce1d4a12dc082c5347d982b50625270c431f82f5
Sztakler <krystian.jasionek@gmail.com>
2022-05-31 23:45:06 +0200

    Init commit.

---
+++ test/test/CMakeFiles/BrainfuckTests.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/krystian/Documents/Programming/PARO/bflang-cpp-dojo/test/BrainfuckTests.cpp" "test/CMakeFiles/BrainfuckTests.dir/BrainfuckTests.cpp.o" "gcc" "test/CMakeFiles/BrainfuckTests.dir/BrainfuckTests.cpp.o.d"
+  "/home/krystian/Documents/Programming/PARO/bflang-cpp-dojo/test/MemoryTests.cpp" "test/CMakeFiles/BrainfuckTests.dir/MemoryTests.cpp.o" "gcc" "test/CMakeFiles/BrainfuckTests.dir/MemoryTests.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/krystian/Documents/Programming/PARO/bflang-cpp-dojo/test/src/CMakeFiles/brainfuck.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
